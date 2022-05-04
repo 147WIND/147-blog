6cf7e4bbdd72c9592ff9622c3797cf8650fd50ee
147WIND <73149828+147WIND@users.noreply.github.com>
2022-05-04 21:52:09 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 export default {
-  title: 'yun yu yuan', // 网站标题
-  nickName: "云与原", // 昵称
-  githubName: 'yunyuyuan', // github账户名
-  githubRepo: 'nuxt2-blog', // 勿修改
+  title: '147’s blog', // 网站标题
+  nickName: "147WIND", // 昵称
+  githubName: '147WIND', // github账户名
+  githubRepo: '147-blog', // 勿修改
   githubBranch: 'master', // 勿修改
   domain: 'https://blog.halberd.cn', // rss域名
-  SEO_description: '云与原的博客，书写值得书写的东西',
-  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,云与原,云与原的博客',
+  SEO_description: '147的博客，记录记忆',
+  SEO_keywords: '147WIND,147\'s blog,147,云龙的博客',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc", // cloudflare的webanalyze
 }
